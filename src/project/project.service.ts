b4174ea355e653023e063a00fbf37926b5f25da4
Khushi <khushijalan1302@gmail.com>
2026-06-02 19:49:52 +0530

    added comments

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -13,11 +13,14 @@ export class ProjectService {
     ) {}
 
     async seed(): Promise<{ dev1: Developer; dev2: Developer }> {
+        //Create Projects
         const [projectA, projectB] = await Promise.all([
             this.projectModel.create({ title: 'Nest CRM'}),
             this.projectModel.create({ title: 'Mongo Analytics'})
         ]);
 
+
+        //Create developers with projects
         const [dev1, dev2] = await Promise.all([                      //developers should have projects
             this.developerModel.create({         
                 name: 'ABC',
@@ -29,8 +32,11 @@ export class ProjectService {
             })
         ])
 
-        await Promise.all([                         //projects should have developers
-            this.projectModel.findByIdAndUpdate(projectA._id, {
+        //PROJECTS DONT KNOW DEVELOPERS YET, SO NEXT WE WILL ASSIGN DEVELOPERS TO PROJECTS
+        //Updating Projects
+        await Promise.all([           //projects should have developers & after we will have dev1,dev2 then only we can insert into projects
+
+            this.projectModel.findByIdAndUpdate(projectA._id, { // we already created above, now just adding developers object
                 $set: { developers: [dev1._id, dev2._id]}
             }),
             this.projectModel.findByIdAndUpdate(projectB._id, {
